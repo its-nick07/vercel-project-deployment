135f905fc5d2c69bc10e524210c2a7ee995e2b44
its-nick07 <nickwilliamsxx@gmail.com>
2024-07-15 13:25:00 +0200

    Fixing the server index

--- server/index.js
+++ server/index.js
@@ -1,35 +1,41 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 8000; // Use process.env.PORT for deployment
-
+const port = 8000;
+const cors = require("cors");
 app.use(cors());
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 mongoose
-  .connect("mongodb+srv://nickwilliamsxx:9pF3W6y2A1tFDzWU@cluster0.0yewz3q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  .connect(
+    "mongodb+srv://nickwilliamsxx:9pF3W6y2A1tFDzWU@cluster0.0yewz3q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log("Connected to the Database MongoDB");
   })
   .catch((error) => {
     console.log("Error connecting to MongoDB", error);
   });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log("Server is running on port 8000");
 });
 
 const Member = require("./models/member");
 
+//Endpoint to register members
 app.post("/addMember", async (req, res) => {
   try {
     const { memberName, memberEmail, phoneNumber } = req.body;
 
+    //Create A New Member
     const newMember = new Member({
+      
+      // memberId,
       memberName,
       memberEmail,
       phoneNumber,
@@ -42,19 +48,19 @@ app.post("/addMember", async (req, res) => {
       member: newMember,
     });
   } catch (error) {
-    console.log("Error registering member", error);
+    console.log("Error regestering member", error);
     res.status(500).json({
-      message: "Failed to add a member",
+      message: "Failed to add member",
     });
   }
 });
 
+//Endpoint to fetch all the members
 app.get("/members", async (req, res) => {
   try {
     const members = await Member.find();
     res.status(200).json(members);
   } catch (error) {
-    console.log("Error retrieving members", error);
     res.status(500).json({ message: "Failed to retrieve the member" });
   }
 });
